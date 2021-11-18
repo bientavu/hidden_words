20c488aa05f6dd84b445d386abebe28715c2dd50
Axel <axel.mesnard@kedgebs.com>
2021-11-18 14:12:48 +0100

    db_insertion updated and working

--- database/db_insertion.py
+++ database/db_insertion.py
@@ -1,15 +1,23 @@
 import boto3
 
 
-def db_insertion():
+def db_insertion(words_dict):
     dynamodb = boto3.resource('dynamodb')
-
     table = dynamodb.Table('battle_word')
+
+    for dictionary in words_dict:
+        table.put_item(
+            Item={
+                'word': dictionary['word'],
+                'definition': dictionary['definition']
+            }
+        )
 
     response = table.get_item(
         Key={
             'word': 'axeltest',
         }
     )
+
     item = response['Item']
     print(item)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from database.db_insertion import db_insertion
 from database.dictionary_parser import WordDownloader, DictionaryCleaner
 
-app = db_insertion()
-
 word_downloader = WordDownloader()
 dict_cleaner = DictionaryCleaner()
 
@@ -11,3 +9,5 @@ dicte_def = word_downloader.get_words_definition(all_words)
 
 clean = dict_cleaner.clean(dicte_def)
 print(clean)
+
+app = db_insertion(clean)
