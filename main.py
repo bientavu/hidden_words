4a7f942b8f7c9c6a9127dab60e82efb247ed591f
Axel <axel.mesnard@kedgebs.com>
2022-01-05 12:53:08 +0100

    main for lambda updated

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
+import json
+import logging
 import boto3
+from botocore.exceptions import ClientError
 from app.pdf_generator import PdfGenerator
-from constants import BUCKET_NAME, OBJECT_KEY
+from constants import BUCKET_NAME
 
-s3 = boto3.client("s3")
-pdf_generator = PdfGenerator(12, 24)
+s3_client = boto3.client("s3")
 
 
 def lambda_handler(event, context):
+    print(f"EVENT:::{event}")
+    print(f"CONTEXT:::{context}")
+
+    grid_size = int(event['grid-size'])
+    words_number = int(event['words-number'])
+
+    pdf_generator = PdfGenerator(words_number, grid_size)
     create_pdf = pdf_generator.generate_pdf()
-    s3.upload_file(create_pdf.filepath, BUCKET_NAME, create_pdf.filename)
+    s3_client.upload_file(
+        create_pdf.filepath,
+        BUCKET_NAME,
+        create_pdf.filename
+    )
+
+    try:
+        response = s3_client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': BUCKET_NAME, 'Key': create_pdf.filename},
+            ExpiresIn=3600
+        )
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    return json.dumps({
+        'statusCode': 200,
+        'body': response
+    })
