1d108c95d910df5d2135301cafe04853eb867945
Axel <axel.mesnard@kedgebs.com>
2022-01-13 11:22:15 +0100

    cleaning

--- database/db_insertion.py
+++ database/db_insertion.py
@@ -1,14 +1,5 @@
 import boto3
 
-# word_downloader = WordDownloader()
-# dict_cleaner = DictionaryCleaner()
-#
-# all_words = word_downloader.get_words_list()
-# dicte_def = word_downloader.get_words_definition(all_words)
-#
-# clean = dict_cleaner.clean(dicte_def)
-# print(clean)
-
 
 def db_insertion(words_dict, table_name):
     """
