6c16b9978fbbdb36a2205c70659eb830f5f9af01
Axel <axel.mesnard@kedgebs.com>
2022-01-13 11:21:43 +0100

    removing some prints

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ s3_client = boto3.client("s3")
 
 
 def lambda_handler(event, context):
-    print(f"EVENT:::{event}")
-    print(f"CONTEXT:::{context}")
-
     grid_size = int(event['grid-size'])
     words_number = int(event['words-number'])
 
