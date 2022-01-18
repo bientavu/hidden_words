5ee644dca95a91ac7634aee204e9ba671506e559
Axel <axel.mesnard@kedgebs.com>
2022-01-18 17:17:25 +0100

    website cleaned and fully working

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -4,3 +4,24 @@ $('select').on('change', function() {
     var newurl = 'https://4is90gmk99.execute-api.eu-west-1.amazonaws.com/production/generate-file?' + $.param(obj);
     $('#add-parameters').attr("href", newurl);
 });
+
+$(function () {
+    $('button').on('click', function () {
+        theURL = $(this).attr('href');
+        $.ajax({
+            type: 'GET',
+            url: theURL,
+            async: false,
+            beforeSend: (xhr) => {
+              if (xhr && xhr.overrideMimeType) {
+                xhr.overrideMimeType('application/json;charset=utf-8');
+              }
+            },
+            dataType: 'json',
+            success: (data) => {
+                var json = JSON.parse(data);
+                window.open(json.body, "_blank");
+            }
+        });
+    });
+})
