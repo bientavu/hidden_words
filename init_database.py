787194c6ecd496ab604a535f0fe2c5984df8e57b
Axel <axel.mesnard@kedgebs.com>
2022-01-18 17:17:56 +0100

    adding some print messages

--- init_database.py
+++ init_database.py
@@ -5,8 +5,14 @@ from database.db_insertion import db_insertion
 word_downloader = WordDownloader()
 dictionary_cleaner = DictionaryCleaner()
 
+print("Scraping all the words...")
 all_words = word_downloader.get_words_list(ALL_LETTERS)
+
+print("Scraping all the words definitions...")
 all_words_and_def = word_downloader.get_words_definition(all_words)
 
+print("Cleaning all the words...")
 clean = dictionary_cleaner.clean(all_words_and_def)
+
+print("Inserting all the words in the DB...")
 db_insertion = db_insertion(clean, TABLE_NAME)
