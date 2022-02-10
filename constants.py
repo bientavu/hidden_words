de4a5eb4394917d1b1ab63040d0667b11ad5821b
Axel <axel.mesnard@kedgebs.com>
2022-02-10 10:24:38 +0100

    removing DSN

--- constants.py
+++ constants.py
@@ -11,7 +11,3 @@ ALL_LETTERS = list(string.ascii_uppercase)
 # S3
 BUCKET_NAME = "hidden-words-resources"
 OBJECT_KEY = "pdf"
-
-# Sentry
-DSN = "https://2bd5743c0fef417aa2423829130ff6dc@o1035883.ingest.sentry.io" \
-      "/6157001 "
